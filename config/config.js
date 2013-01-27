907d896f8ea8ff3f55b65f86523367121a3583e1
Ryan Endacott <rzeg24@gmail.com>
2013-01-27 16:58:42 -0600

    Config with Xeround db info

---
+++ config/config.js
@@ -0,0 +1,10 @@
+// Config
+
+module.exports = {
+    db: {
+        database: 'InterestMap',
+        host: 'instance37525.db.xeround.com:5659',
+        username: 'interestmapdb',
+        password: 'thebestmapever'
+    }
+}

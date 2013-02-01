d8aa6ce8820298d82a6e42ab3d02960061291ad7
Ryan Endacott <rzeg24@gmail.com>
2013-01-31 23:07:49 -0600

    Get all follower IDs function

--- tweetcrawler.js
+++ tweetcrawler.js
@@ -2,11 +2,22 @@
 // Hopefully a successful refactoring of app.js
 
 var Step = require('step'),
-  db = require('./db');
+  db = require('./db'),
+  twitter = require('ntwitter');
 
 
 
 var tweetCrawler = {};
+
+
+var twit = new twitter({
+  consumer_key: 'n5WyKBVmWudsps8X3GLlaw',
+  consumer_secret: 'itjK8WdQkxRMjDB6SbjatovwrxgQhXXx2uLZvfz9Po',
+  access_token_key: '1126351788-uZza6Zb6IKLQ3xcdXg8Moegr0OeA6FIljLZQB6U',
+  access_token_secret: 'rrqAlFwbWa86KideKwOxlGjtPmPEuP7TR623ivOc'
+});
+
+var LIMIT_USER_SEARCH = 100;
 
 // Global variables to hold stuff to keep from
 // checking database if we already have
@@ -34,6 +45,16 @@ tweetCrawler.run = function() {
 
       // Get first interest, can later be done by priority (e.g. date last updated)
       this(null, interests.splice(0,1))
+    },
+    function getFollowerIDs(err, interest) {
+      if(err) throw err;
+
+      // Build up a group so we get ids of all followers of each topic
+      var group = this.group();
+
+      interest.twitter_names.forEach(function (twitter_name) {
+        twit.get('/followers/ids.json', {screen_name: twitter_name}, group());
+      })
     }
 
 

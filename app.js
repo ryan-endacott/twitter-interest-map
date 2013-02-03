2499e1d01decec8f870ebf6a325d84744b1a0858
Dan Silver <dannysilver3@gmail.com>
2013-02-02 22:50:56 -0600

    Removed old functions within app.js.  Run with node app again

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , twitter = require('ntwitter')
-  , request = require('request')
-  , db = require('./db')
-  , path = require('path');
+     , routes = require('./routes')
+     , http = require('http')
+     , path = require('path');
 
 var app = express();
 
@@ -26,7 +17,7 @@ app.configure(function(){
   app.use(express.session());
   app.use(app.router);
   app.use(require('stylus').middleware(__dirname + '/public'));
-  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.static(__dirname, 'public'));
 });
 
 app.configure('development', function(){
@@ -39,109 +30,4 @@ http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
 
-var twit = new twitter({
-  consumer_key: 'n5WyKBVmWudsps8X3GLlaw',
-  consumer_secret: 'itjK8WdQkxRMjDB6SbjatovwrxgQhXXx2uLZvfz9Po',
-  access_token_key: '1126351788-uZza6Zb6IKLQ3xcdXg8Moegr0OeA6FIljLZQB6U',
-  access_token_secret: 'rrqAlFwbWa86KideKwOxlGjtPmPEuP7TR623ivOc'
-});
-var blacklist = ['world', 'theworld'];
-
-var twitterUsernames = ['nodejs'];
-var LIMIT_USER_SEARCH = 100;
-var currentUsername = 0;
-
-var numbers = /[0-9]/; //prevent numbers from being in one of the locations
-
-function getFollowers(screen_name) {
-  var followers = [];
-  twit.get('/followers/ids.json', {screen_name: screen_name}, function(err, data) {
-    if (err) console.log(err);
-	if (!data) return;
-    if (data.ids.length > LIMIT_USER_SEARCH) {
-      data.ids.splice(0,data.ids.length - LIMIT_USER_SEARCH);
-    }
-    twit.get('/users/lookup.json', {user_id: data.ids.join()}, function(err, users) {
-      if (err) console.log(err);
-      if (!users) return;
-	  users.forEach(function(user) {
-        if (user && user.location && (blacklist.indexOf(user.location) == -1 ) && !numbers.test(user.location)) {
-          followers.push({location: user.location, uid: user.id});
-        }
-	  });
-      findUser(followers)
-	});
-  });
-}
-
-function findUser(followers) {
-	followers.forEach(function(follower) {
-		db.user.find({twitter_id:follower.uid}).exec( function (err, user) {
-		  if (user.length) {
-		  
-		  } else { 
-        console.log('should always reach here.')
-
-        addLocationToDB(follower.location, function(err, newLoc) {
-          console.log('line 86');
-		  console.log(follower.uid);
-          var user = new db.user({twitter_id: follower.uid, location: newLoc._id});
-          user.save();
-        })
-		  }
-		});
-	});
-	console.log(followers);
-   currentUsername++;
-   if (currentUsername != twitterUsernames.length) {
-	getFollowers(twitterUsernames[currentUsername]);
-   } else {
-	 console.log("Done");
-   }
-}
-
-//uncomment the following line to query twitter for the folower uid's and locations
-getFollowers(twitterUsernames[0]);
-
-//we can use the google maps geolocation api to convert location strings to objects with city, state, and country strings
-//this example functions takes an address and writes an object with the state and city to the console
-function getLocation(address, callback) {
-	request({url: 'http://maps.googleapis.com/maps/api/geocode/json', qs: {address:address, sensor: false}}, function (error, response, body) {
-	  if (!error && response.statusCode == 200) {
-		var location = {raw: address};
-		if (JSON.parse(body).results.length) {
-		address_components = JSON.parse(body).results[0].address_components;
-		if (address_components){
-		  for (var i=0;i<address_components.length;i++) {
-			if (address_components[i].types.indexOf('locality') != -1) {
-			  location.city = address_components[i].long_name;
-			} else if (address_components[i].types.indexOf('administrative_area_level_1') != -1) {
-			  location.state = address_components[i].long_name;
-			} else if (address_components[i].types.indexOf('country') != -1) {
-			  location.country = address_components[i].long_name;
-			}
-		  }
-		}
-    }
-
-		callback(location);
-	  }
-	})
-}
-function addLocationToDB(rawLoc, callback) {
-  console.log('Execution just entered addLocationToDB');
-  db.location.findOne({raw: rawLoc}, function(err, location) {
-    if (!err && location) return;
-    else { // Add location if it didn't exist
-      getLocation(rawLoc, function(location) {
-		if (location) {
-			var newLoc = new db.location(location);
-			newLoc.save(callback);
-		}
-      })
-    }
-  });
-}
-
-
-
+require('./tweetcrawler').run();

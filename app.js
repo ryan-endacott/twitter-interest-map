79d66398a4ba9b2d7f47b4caecc4e2c181d7a0fd
Dan Silver <dannysilver3@gmail.com>
2013-02-08 21:54:29 -0600

    Removed all front end components of the app

--- app.js
+++ app.js
@@ -1,31 +1,18 @@
 var express = require('express')
-     , routes = require('./routes')
      , http = require('http')
-	 , db = require('./db')
-     , path = require('path');
+	 , db = require('./db');
 
 var app = express();
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
   app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser('your secret here'));
-  app.use(express.session());
   app.use(app.router);
-  app.use(require('stylus').middleware(__dirname + '/public'));
-  app.use(express.static(__dirname, 'public'));
 });
 
 app.configure('development', function(){
   app.use(express.errorHandler());
 });
-
-app.get('/', routes.index);
 
 //an easy way to add interests while we are developing the app
 //ex: http://localhost:3000/interest/add/news?twitter_names=cnn,msnbc,FoxNews

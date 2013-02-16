40bc931da503c7c38d706319db4b41f7833fbeba
Dan Silver <dannysilver3@gmail.com>
2013-02-15 22:07:12 -0600

    No db saving yet. It currently looks up the top twitter users for a given array of topics :)

---
+++ searchInterests.js
@@ -0,0 +1,41 @@
+var async = require('async'),
+      db = require('./db'),
+      twitter = require('ntwitter')
+var twit = new twitter({
+  consumer_key: 'n5WyKBVmWudsps8X3GLlaw',
+  consumer_secret: 'itjK8WdQkxRMjDB6SbjatovwrxgQhXXx2uLZvfz9Po',
+  access_token_key: '1126351788-uZza6Zb6IKLQ3xcdXg8Moegr0OeA6FIljLZQB6U',
+  access_token_secret: 'rrqAlFwbWa86KideKwOxlGjtPmPEuP7TR623ivOc'
+})
+var completed_interests = [] //array of screen_name arrays
+var interests = ['node js', 'CNN']
+var currentInterest = 0
+var findTwitterUsers = function() {
+async.waterfall([
+  function(callback){
+    callback(null, interests[currentInterest]);
+  },
+  function(interest, callback) {
+    twit.get('/users/search.json', {q: interest}, function(err, data) {
+      callback(err, data)
+    });
+  }, function(users, callback) {
+      async.map(users.splice(0,7), function(user, callback) {
+        callback(null, user.screen_name);
+      }, function(err, screen_names){
+        callback(null, screen_names)
+    })
+  }, function(screen_names, callback) {
+    console.log("Top twitter users for the interest '" + interests[currentInterest]+"'");
+    console.log(screen_names);
+    callback(null, screen_names)
+  }
+], function(err, screen_names) {
+  if (err) console.log(err)
+  if (currentInterest < interests.length - 1) {
+    currentInterest++;
+    findTwitterUsers()
+  }
+ });
+}
+findTwitterUsers();
